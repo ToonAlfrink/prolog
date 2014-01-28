2928d3f08f11ce7d79966c0156d2468d6b6a165d
Toon Alfrink <toon.alfrink@gmail.com>
2014-01-28 18:21:15 +0100

    schoonheidsprijs winnen

--- graycode.pl
+++ graycode.pl
@@ -1,19 +1,17 @@
+% isEqual/2: true if first argument matches second argument
+isEqual(A,A).
+
+% prepend/3: concatenates lists
 prepend([],L,L).
 prepend([H|T],L2,[H|L3]) :- prepend(T,L2,L3).
 
+% prefix/3: prefixes an element to each lists in a list of lists
 prefix(E,[],[]).
 prefix(E,[H|T],[H1|L]) :- 
-        prepend([E],H,H1),
+        isEqual([E|H],H1),
         prefix(E,T,L).
 
-append([X],L1,L2) :- 
-        last(L2,X),
-        prepend(L1,[X],L2).
-
-append([H|T],L1,L2) :- 
-        append([H],L1,X),
-        append(T,X,L2).
-
+% gray/2: gets the gray code of a given bit length
 gray(0,[[]]).
 gray(N,L) :-
         N>0,
@@ -28,24 +26,19 @@ gray(N,L) :-
 difference([Head1|EqualTail],[Head2|EqualTail],Head2).
 difference([EqualHead|Tail1],[EqualHead|Tail2],Out) :- difference(Tail1,Tail2,Out).
 
-isEqual(A,A).
-
 % turns/2: turns a gray code sequence into a turtle sequence
 turns([Element],[]).
 turns([First,Next|GrayTail],[TurnHead,fd|TurnTail]) :-
         difference(First,Next,ChangedDigit),
         (isEqual(ChangedDigit,1) -> isEqual(TurnHead,lt);true),
         (isEqual(ChangedDigit,0) -> isEqual(TurnHead,rt);true),
-        prepend([Next],GrayTail,NextTail),
-        turns(NextTail,TurnTail).
+        turns([Next|GrayTail],TurnTail).
         
-
-% getdisc/3: gets the index of the bit that changes
+% getdisc/3: gets the index of the bit that changes, which corresponds with the disc to move
 getdisc([Head1|EqualTail],[Head2|EqualTail],Out) :- length(EqualTail,X),Out is X + 1.
-getdisc([EqualHead|Tail1],[EqualHead|Tail2],Out) :-
-        diffindex(Tail1,Tail2,Out).
+getdisc([EqualHead|Tail1],[EqualHead|Tail2],Out) :- getdisc(Tail1,Tail2,Out).
 
-% move/4 with disc 1
+% move/4 with disc 1, which needs special treatment
 move(P,1,[L1,L2,L3],New) :-
         (isEqual(P,0) -> %even parity: smallest disc goes left-mid-right
             ((isEqual(L1,[1|T1]) -> isEqual(New,[T1,[1|L2],L3]);true),
@@ -63,7 +56,7 @@ move(P,Schijf,[L1,L2,L3],New) :-
         update(Schijf,L3,L3a),
         isEqual(New,[L1a,L2a,L3a]).
 
-% update/3: remove the disc if it's there, add it if there's room for it.
+% update/3: remove the disc if it's there, add it if it fits on the stack
 update(Schijf, LijstIn, LijstUit) :-
         (isEqual(LijstIn,[Schijf|Tail]) -> isEqual(LijstUit, Tail));
         ((isEqual(LijstIn,[]) ; (isEqual(LijstIn,[Head|Tail]), Schijf<Head)) ->
@@ -77,10 +70,8 @@ towers(N,[[Left,[],[]]|L]) :-
         findall(X,between(1,N,X),Left),
         towers(P,GraySeq,[Left,[],[]],L).
 
-% towers/4 non-recursive
+% towers/4: gets the steps taken in a towers of hanoi game.
 towers(P,[LastGray],Vorige,[]).
-
-% towers/4 recursive, gets the steps taken in a towers of hanoi game.
 towers(P,[GrayHead,NextGray|GraySeq],Vorige,[SpelHead|SpelSituaties]) :-
         getdisc(GrayHead,NextGray,Schijf),
         move(P,Schijf,Vorige,SpelHead),
